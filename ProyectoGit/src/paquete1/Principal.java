2f1739cd65c7de42908d19fbe68a2138e15c725e
ricar <ricar@HUAWEI-RICARDO>
2024-03-11 20:43:07 +0100

    Versión inicial del programa 1.0

---
+++ ProyectoGit/src/paquete1/Principal.java
@@ -0,0 +1,11 @@
+package paquete1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Probando repositorio Git");
+
+	}
+
+}
